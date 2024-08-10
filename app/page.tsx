915f256b77da398023f1dfe0055030b4ad55d3f0
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-08-11 00:35:58 +0530

    Project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1 className="bg-blend-color-dodge bg-red-600">Hello</h1>
+      <Button>Click Me</Button>
+    </>
+  );
 }
