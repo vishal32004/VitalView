275620962a6e9755e2a3c68f51eccd110748d992
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-08-11 01:05:18 +0530

    Modifed Home page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="bg-blend-color-dodge bg-red-600">Hello</h1>
-      <Button>Click Me</Button>
-    </>
+    <div className="flex h-screen max-h-screen">
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding-patient"
+        title="onboarding-patient"
+        className="side-img max-w-[50%]"
+      />
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={"/assets/icons/logo.svg"}
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12  h-10 w-fit"
+            title="vital-view"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left flex">
+              © 2024 VitalView
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
