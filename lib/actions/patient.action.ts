3fc4116db0fcc2e1f365346127dc0179462dfa83
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-08-18 01:26:06 +0530

    added register patient action

--- lib/actions/patient.action.ts
+++ lib/actions/patient.action.ts
@@ -1,6 +1,7 @@
 "use server"
 import { Query, ID } from "node-appwrite";
 import { parseStringify } from "../utils";
+import { InputFile } from "node-appwrite/file";
 import {
     NEXT_PUBLIC_BUCKET_ID,
     DATABASE_ID,
@@ -39,13 +40,51 @@ export async function createUser(user: CreateUserParams) {
 // GET USER
 export const getUser = async (userId: string) => {
     try {
-      const user = await users.get(userId);
-
-      return parseStringify(user);
+        const user = await users.get(userId);
+        return parseStringify(user);
     } catch (error) {
-      console.error(
-        "An error occurred while retrieving the user details:",
-        error
-      );
+        console.error(
+            "An error occurred while retrieving the user details:",
+            error
+        );
     }
-  };
+};
+
+// REGISTER PATIENT
+export const registerPatient = async ({
+    identificationDocument,
+    ...patient
+}: RegisterUserParams) => {
+    try {
+        // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+        let file;
+        if (identificationDocument) {
+            const inputFile =
+                identificationDocument &&
+                InputFile.fromBuffer(
+                    identificationDocument?.get("blobFile") as Blob,
+                    identificationDocument?.get("fileName") as string
+                );
+
+            file = await storage.createFile(NEXT_PUBLIC_BUCKET_ID!, ID.unique(), inputFile);
+        }
+
+        // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+        const newPatient = await databases.createDocument(
+            DATABASE_ID!,
+            PATIENT_COLLECTION_ID!,
+            ID.unique(),
+            {
+                identificationDocumentId: file?.$id ? file.$id : null,
+                identificationDocumentUrl: file?.$id
+                    ? `${NEXT_PUBLIC_ENDPOINT}/storage/buckets/${NEXT_PUBLIC_BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+                    : null,
+                ...patient,
+            }
+        );
+
+        return parseStringify(newPatient);
+    } catch (error) {
+        console.error("An error occurred while creating a new patient:", error);
+    }
+};
